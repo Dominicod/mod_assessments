12dd01edeeed6ea35532f31b7fd94185d3d26ede
dominicod <dominicodonnell99@gmail.com>
2022-07-20 12:26:41 -0500

    feat: create logic for passenger class, passing spec files

--- vehicles_passengers_2207/spec/passenger_spec.rb
+++ vehicles_passengers_2207/spec/passenger_spec.rb
@@ -2,49 +2,49 @@ require './lib/passenger'
 
 RSpec.describe Passenger do
     describe ".passenger_initialize" do
-        context "passenger object is instance of class" do
-            before(:each) do
-                charlie = Passenger.new({"name" => "Charlie", "age" => 18}) 
-                taylor = Passenger.new({"name" => "Taylor", "age" => 12})
-            end
+        before(:each) do
+            @charlie = Passenger.new({"name" => "Charlie", "age" => 18}) 
+            @taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+        end
 
+        context "passenger object is instance of class" do
             it 'exists' do
-                expect(charlie).to be_an_instance_of(Passenger)
-                expect(taylor).to be_an_instance_of(Passenger)
+                expect(@charlie).to be_an_instance_of(Passenger)
+                expect(@taylor).to be_an_instance_of(Passenger)
             end
         end
 
         context "passenger object returns instance variables" do
             it 'returns name' do
-                expect(charlie.name).to eq("Charlie")
-                expect(taylor.name).to eq("Taylor")
+                expect(@charlie.name).to eq("Charlie")
+                expect(@taylor.name).to eq("Taylor")
             end
 
             it 'returns age' do
-                expect(charlie.age).to eq(18)
-                expect(taylor.age).to eq(12)
+                expect(@charlie.age).to eq(18)
+                expect(@taylor.age).to eq(12)
             end
         end
 
         context "returns boolean if method adult? is executed" do
-            it 'returns true for charlie' do
-                expect(charlie.adult?).to eq(true)
+            it 'returns true for @charlie' do
+                expect(@charlie.adult?).to eq(true)
             end
 
-            it 'returns false for taylor' do
-                expect(taylor.adult?).to eq(false)
+            it 'returns false for @taylor' do
+                expect(@taylor.adult?).to eq(false)
             end
         end
 
         context "returns boolean for driver? allow reassigning of driver?" do
             it 'returns false, nobody is driving' do
-                expect(charlie.driver?).to eq(false)
-                expect(taylor.driver?).to eq(false)
+                expect(@charlie.driver?).to eq(false)
+                expect(@taylor.driver?).to eq(false)
             end
 
-            it 'returns true for charlie given .drive' do
-                charlie.drive
-                expect(charlie.driver?).to eq(true)
+            it 'returns true for @charlie given .drive' do
+                @charlie.drive
+                expect(@charlie.driver?).to eq(true)
             end
         end
     end
