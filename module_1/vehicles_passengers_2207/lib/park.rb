5724c65212a56c340e40fccffc215a91f00814be
dominicod <dominicodonnell99@gmail.com>
2022-07-21 11:49:02 -0500

    feat: add iteration 3

---
+++ module_1/vehicles_passengers_2207/lib/park.rb
@@ -0,0 +1,28 @@
+class Park
+    attr_reader :name,
+                :admission,
+                :vehicles
+    def initialize(name, admission)
+        @name = name
+        @admission = admission
+        @vehicles = []
+    end
+
+    def enter(vehicle)
+        @vehicles << vehicle
+    end
+
+    def passengers
+        arr = @vehicles.map {|vehicle| vehicle.passengers.map {|passenger| passenger.name} }
+        new_arr = arr[0] | arr[1]
+    end
+
+    def calculate_revenue 
+        @vehicles.map {|vehicle| vehicle.passengers.find_all {|passenger| passenger.adult? }.count * 20}.sum
+    end
+
+    def revenue_by_vehicle
+        revenue_hash = {}
+        @vehicles.map {|vehicle| revenue_hash.store(vehicle, nil)}
+    end
+end
